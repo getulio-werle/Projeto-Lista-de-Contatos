8c1f414c18a124e1c4504d27c8857fbe1a5eb61d
Getulio <getuliowerle10@gmail.com>
2024-07-10 09:46:48 -0400

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// configuration file
+define('MYSQL_CONFIG', [
+        'host' => 'localhost',
+        'database' => 'php_pdo_contatos',
+        'username' => 'user_php_pdo_contatos',
+        'password' => 'afdsaf23423$%6'
+    ]);
